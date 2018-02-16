527b596ac89aebce9f740a327aa4f9ef867f602d
Abey Onalaja <abey.onalaja@gmail.com>
2018-02-16 01:18:14 +0000

    Registration form html

--- lib/mango_web/controllers/registration_controller.ex
+++ lib/mango_web/controllers/registration_controller.ex
@@ -1,8 +1,10 @@
 defmodule MangoWeb.RegistrationController do
   use MangoWeb, :controller
+  alias Mango.CRM
 
   def new(conn, _) do
-    render(conn, "new.html")
+    changeset = CRM.build_customer()
+    render(conn, "new.html", changeset: changeset)
   end
 
 end
